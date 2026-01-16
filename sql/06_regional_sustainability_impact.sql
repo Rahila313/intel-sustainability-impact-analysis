72b32ac2e0f7bd19f55f58adef888331bec919fb
Rahila Jafari <rjafari3@asu.edu>
2026-01-15 19:04:42 -0700

    Add regional sustainability impact analysis query

---
+++ sql/06_regional_sustainability_impact.sql
@@ -0,0 +1,10 @@
+SELECT
+  i.region,
+  COUNT(*) AS total_devices,
+  ROUND(AVG(i.energy_savings_yr), 2) AS avg_energy_savings,
+  ROUND(AVG(i.co2_saved_kg_yr) / 1000, 2) AS avg_co2_saved_tons
+FROM
+  intel.device_data d
+  JOIN intel.impact_data i ON d.device_id = i.device_id
+GROUP BY
+  i.region;
